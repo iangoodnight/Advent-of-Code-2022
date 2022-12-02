89b543a3d04d38531c30a582f2aab9ce01ea3413
Ian Goodnight <goodnight.ian@gmail.com>
2022-12-02 00:26:30 -0500

    day 1 solutions

---
+++ README.md
@@ -0,0 +1,11 @@
+# [Advent of Code 2022][0]
+
+Tracking my progress through this year's [Advent of Code][0].  Problem
+statements are reproduced, solutions are my own.
+
+| Day   | Stars           |
+| ----- | --------------- |
+| [1]   | &#9733; &#9733; |
+
+[0]: <https://adventofcode.com/2022/about> "About Advent of Code"
+[1]: </day-1/ch-1/README.md> "Day 1 problem statement"
---
+++ day-1/ch-2.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+=begin comment
+
+<https://adventofcode.com/2022/day/1> "Advent of Code - Day 1"
+
+## Part Two
+
+By the time you calculate the answer to the Elves' question, they've already
+realized that the Elf carrying the most Calories of food might eventually
+**run out of snacks**.
+
+To avoid this unacceptable situation, the Elves would instead like to know the
+total Calories carried by the **top three** Elves carrying the most Calories.
+That way, even if one of those Elves runs out of snacks, they still have two
+backups.
+
+In the example above, the top three Elves are the fourth Elf (with `24000`
+Calories), then the third Elf (with `11000` Calories), then the fifth Elf (with
+`10000` Calories). The sum of the Calories carried by these three elves is 
+`45000`.
+
+Find the top three Elves carrying the most Calories.
+**How many Calories are those Elves carrying in total?**
+
+=end comment
+=cut
+
+use strict;
+use warnings;
+use English qw( -no_match_vars );
+use List::Util qw ( sum );
+use Term::ANSIColor;
+
+our $VERSION = 0.0.1;
+
+if ( !( -p STDIN ) ) {
+    print color('yellow'), "Usage:\n", color('reset') or die $!;
+    print "\t\$> ", color('green'), "cat input.txt | ./ch-2.pl\n",
+      color('reset') or die $!;
+    exit 0;
+}
+
+my $data = do { local $RS = undef; <> };
+
+my @elves = map {
+    [ split / \n /x ]    ## no critic (RegularExpressions)
+} split / \n \n /x, $data;    ## no critic (RegularExpressions)
+
+my @totals = reverse sort { $a <=> $b } map { sum @{$_}; } @elves;
+
+my @top_3        = @totals[ 0 .. 2 ];
+my $top_3_totals = sum @top_3;
+
+print 'Top three totaled: ', color('green'), $top_3_totals, "\n",
+  color('reset') or die $!;
